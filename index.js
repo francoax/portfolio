b2a556f5760f78541c961c0f13df8fc32f8c672a
francoax <francoa.duarte2001@gmail.com>
2023-12-26 13:43:24 -0300

    lang handler initialized.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+import { english, spanish } from './lang.js'
+
+const btnLang = document.getElementById('labelSwitch')
+
+const btn = document.getElementById('switch')
+
 window.onload = () => {
+
+  handleLanguageOnEntry()
+
   const btns = ['homeBtn', 'aboutBtn', 'proyectBtn', 'contactBtn']
 
   const containers = {
@@ -15,4 +24,56 @@ window.onload = () => {
     })
   });
 }
+
+const handleLanguageOnEntry = () => {
+  if(localStorage.getItem('lang') === 'spanish') {
+    changeLanguage(spanish)
+    btn.checked = false
+
+    btnLang.setAttribute('data-lang', 'spanish')
+  } else {
+    changeLanguage(english)
+    btn.checked = true
+
+    btnLang.setAttribute('data-lang', 'english')
+  }
+}
+
+const changeLanguage = (lang) => {
+  Object.entries(lang).forEach(([section, values]) => {
+    Object.entries(values).forEach(([element, content]) => {
+      document.querySelector(`.${element}`).innerHTML = content;
+    })
+  })
+}
+
+var onCooldown = false
+
+const handleClick = (e) => {
+  if(!onCooldown) {
+    onCooldown = true
+    const currentLanguage = e.target.getAttribute('data-lang');
+
+    if(currentLanguage === 'spanish') {
+      localStorage.setItem('lang', 'english')
+      e.target.setAttribute('data-lang', 'english')
+
+      btn.checked = true
+
+      changeLanguage(english)
+    } else {
+      localStorage.setItem('lang', 'spanish')
+      e.target.setAttribute('data-lang', 'spanish')
+
+      btn.checked = false
+
+      changeLanguage(spanish)
+    }
+    setTimeout(() => onCooldown = false, 5000)
+  }
+}
+
+btnLang.addEventListener('click', (e) => {
+  handleClick(e)
+})
 
